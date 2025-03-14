db191a9f1fcaf4ca55ae225ae271ffbe48c43981
Ivan Toropygin <itori86@mail.ru>
2025-03-14 03:02:16 +0300

    work with nav menu

--- app/src/main/java/com/example/kinopoiskpopularmovies/ui/home/MainBottomActivity.kt
+++ app/src/main/java/com/example/kinopoiskpopularmovies/ui/home/MainBottomActivity.kt
@@ -2,14 +2,15 @@ package com.example.kinopoiskpopularmovies.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.example.kinopoiskpopularmovies.R
 import com.example.kinopoiskpopularmovies.databinding.ActivityMainBottomBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainBottomActivity : AppCompatActivity() {
-
+    private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBottomBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,10 +18,26 @@ class MainBottomActivity : AppCompatActivity() {
         binding = ActivityMainBottomBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main_bottom) as NavHostFragment
-        val navController = navHostFragment.navController
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_main_bottom) as NavHostFragment
 
-        val navView: BottomNavigationView = binding.navView
-        navView.setupWithNavController(navController)
+        navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when(destination.id) {
+                R.id.movieDetailsFragment -> hideBottomNav()
+                else -> showBottomNav()
+            }
+        }
+    }
+
+    private fun hideBottomNav() {
+        binding.navView.animate().translationY(binding.navView.height.toFloat()).duration = 300
+        binding.navView.isGone = true
+    }
+
+    private fun showBottomNav() {
+        binding.navView.isVisible = true
+        binding.navView.animate().translationY(0f).duration = 300
     }
 }
